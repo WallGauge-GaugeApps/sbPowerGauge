c8182e5cdfcfb0eaa8e583e0aa3f500a4da0678c
John Rucker <John.Rucker@Solar-Current.com>
2019-01-02 10:18:39 -0600

    ed

--- postInstall/installAsService.sh
+++ postInstall/installAsService.sh
@@ -7,6 +7,5 @@ echo "systemctl enable sbPowerGauge.service"
 sudo systemctl enable sbPowerGauge.service
 echo "systemctl start sbPowerGauge.service"
 sudo systemctl start sbPowerGauge.service
-echo "systemctl status sbPowerGauge.service"
-sudo systemctl status sbPowerGauge.service
 echo "NPM Post install shell is complete."
+echo "Sevice should be installed and running.  Check config."
