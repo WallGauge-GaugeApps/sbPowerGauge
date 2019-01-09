3751a605b3c22ec510a4789c99ab36d5fa66a6d5
John Rucker <John.Rucker@Solar-Current.com>
2019-01-09 05:23:45 -0600

    moved ble to 2nd class

--- configManager.js
+++ configManager.js
@@ -12,17 +12,15 @@ if (fs.existsSync(modifiedConfigFilePath)){
 
 var Config = {...defaultGaugeConfig, ...modifiedConfigMaster}
 
-var bPrl = new BLEperipheral(Config.dBusName, Config.uuid, bleMain, false);
+var bleIface = new BleIface();
 
 class gaugeConfig extends EventEmitter{
     constructor(){
         super();
         this.descripition = Config.descripition;
-        this.uuid = Config.uuid;
-        this.dBusName = Config.dBusName;
         this.calibrationTable = Config.calibrationTable;
         this.gaugeIrAddress = Config.gaugeIrAddress;
-        this.webBoxIP = Config.webBoxIP;
+        this.webBoxIP = bleIface.webBoxIP;
     };
 
     setWebBoxIP(ipAdd = '10.1.1.5'){
@@ -33,6 +31,45 @@ class gaugeConfig extends EventEmitter{
     };
 };
 
+class BleIface extends EventEmitter{
+    constructor(){
+        super();
+        this.dBusName = Config.dBusName;
+        this.uuid = Config.uuid;
+        this.bPrl = new BLEperipheral(this.dBusName, this.uuid, this.bleMain, false);
+        this.webBoxIP = Config.webBoxIP;
+    }
+
+    bleMain(DBus){
+        var cfg = new gaugeConfig();
+        bPrl.logCharacteristicsIO = true;
+        console.log('Initialize charcteristics...')
+        var webBoxIp = bPrl.Characteristic('00000001-fe9e-4f7b-b56a-5f8294c6d817', 'webBoxIp', ["encrypt-read","encrypt-write"]);
+    
+        console.log('Registering event handlers...');
+        webBoxIp.on('WriteValue', (device, arg1)=>{
+            console.log(device + ', has set new IP Address of ' + arg1);
+            webBoxIp.setValue(arg1);
+            var x = arg1.toString('utf8');
+            cfg.setWebBoxIP(x);
+            saveItem({webBoxIP:ipAdd});
+            this.webBoxIP = Config.webBoxIP;
+            console.log('firing "Update" event...');
+            this.emit('Update');
+
+
+
+        });
+    
+        console.log('setting default characteristic values...');
+        webBoxIp.setValue(cfg.webBoxIP);
+    };
+
+
+}
+
+/*
+var bPrl = new BLEperipheral(Config.dBusName, Config.uuid, bleMain, false);
 function bleMain(DBus){
     var cfg = new gaugeConfig();
     bPrl.logCharacteristicsIO = true;
@@ -50,6 +87,7 @@ function bleMain(DBus){
     console.log('setting default characteristic values...');
     webBoxIp.setValue(cfg.webBoxIP);
 };
+*/
 
 function saveItem(itemsToSaveAsObject){
     console.log('saveItem called with:');
