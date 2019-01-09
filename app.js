ed1343b0bc1160a49ec6dad9718410f40af0fcd1
John Rucker <John.Rucker@Solar-Current.com>
2019-01-09 07:24:51 -0600

    ed

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ config.on('Update', ()=>{
     console.log('New update event has fired.  Reloading gauge objects...');
     console.log('The webBoxIP = ' + config.webBoxIP);
 
-    //tx = new irTransmitter(config.gaugeIrAddress, config.calibrationTable);
-    //solarData =  new sunnyBoyWebBox(config.webBoxIP);
+    tx = new irTransmitter(config.gaugeIrAddress, config.calibrationTable);
+    solarData =  new sunnyBoyWebBox(config.webBoxIP);
 });
