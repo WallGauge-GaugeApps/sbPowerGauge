905fff8796b9d7c70431e5f101b0d6a324a0b658
John Rucker <John.Rucker@Solar-Current.com>
2019-01-11 09:30:38 -0600

    ed

--- MyAppManager.js
+++ MyAppManager.js
@@ -1,5 +1,18 @@
 const AppManager =      require('./appManager.js');
 
 class myAppManager extends AppManager(){
-    
+    bleMyConfig(){
+        console.log('setting up my characteristics');
+        var webBoxIp =      bPrl.Characteristic('00000010-fe9e-4f7b-b56a-5f8294c6d817', 'webBoxIp', ["encrypt-read","encrypt-write"]);
+
+        webBoxIp.on('WriteValue', (device, arg1)=>{
+            console.log(device + ', has set new IP Address of ' + arg1);
+            webBoxIp.setValue(arg1);
+            var x = arg1.toString('utf8');
+            self.webBoxIP=x;
+            saveItem({webBoxIP:x});
+        });
+
+        webBoxIp.setValue(Config.webBoxIP);
+    }
 }
--- appManager.js
+++ appManager.js
@@ -65,18 +65,7 @@ class gaugeConfig extends EventEmitter{
     };    
 
     bleMyConfig(){
-        console.log('setting up my characteristics');
-        var webBoxIp =      bPrl.Characteristic('00000010-fe9e-4f7b-b56a-5f8294c6d817', 'webBoxIp', ["encrypt-read","encrypt-write"]);
-
-        webBoxIp.on('WriteValue', (device, arg1)=>{
-            console.log(device + ', has set new IP Address of ' + arg1);
-            webBoxIp.setValue(arg1);
-            var x = arg1.toString('utf8');
-            self.webBoxIP=x;
-            saveItem({webBoxIP:x});
-        });
-
-        webBoxIp.setValue(Config.webBoxIP);
+        console.log('No user specfic configuraton for appManger using default characteristics only.');
     }
 
     bleMasterConfig(DBus){
