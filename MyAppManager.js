a908ae54723cd5c348199629c8f6b341ef034f66
John Rucker <John.Rucker@Solar-Current.com>
2019-01-11 13:07:57 -0600

    ed

--- MyAppManager.js
+++ MyAppManager.js
@@ -2,14 +2,14 @@ const AppManager = require("./appManager.js");
 
 class myAppManager extends AppManager{
     bleMyConfig(){
-        console.log('setting up sbPowerGauge specfic characteristics and config.');
-        var webBoxIp = self.bPrl.Characteristic('00000010-fe9e-4f7b-b56a-5f8294c6d817', 'webBoxIp', ["encrypt-read","encrypt-write"]);
+        console.log('Setting up sbPowerGauge specfic characteristics and config.');
+        var webBoxIp = this.bPrl.Characteristic('00000010-fe9e-4f7b-b56a-5f8294c6d817', 'webBoxIp', ["encrypt-read","encrypt-write"]);
 
         webBoxIp.on('WriteValue', (device, arg1)=>{
             console.log(device + ', has set new IP Address of ' + arg1);
             webBoxIp.setValue(arg1);
             var x = arg1.toString('utf8');
-            self.webBoxIP=x;
+            this.webBoxIP=x;
             saveItem({webBoxIP:x});
         });
 
