406256fb3a043a4b6bd5e6519f3f179157eaa136
John Rucker <John.Rucker@Solar-Current.com>
2019-01-12 07:03:10 -0600

    ed

--- appManager.js
+++ appManager.js
@@ -13,7 +13,7 @@ if (fs.existsSync(modifiedConfigFilePath)){
 
 var Config = {...defaultGaugeConfig, ...modifiedConfigMaster};
 
-var gTx = new irTransmitter(Config.gaugeIrAddress, Config.calibrationTable);
+//var gTx = new irTransmitter(Config.gaugeIrAddress, Config.calibrationTable);
 
 //var bPrl;
 var self;
@@ -26,19 +26,26 @@ class gaugeConfig extends EventEmitter{
         this.descripition = Config.descripition;
         this.calibrationTable = Config.calibrationTable;
         this.gaugeIrAddress = Config.gaugeIrAddress;
+        this.uuid = Config.uuid
+        this.dBusName = Config.dBusName
         this.status = 'ipl, ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString();
         this.value = 'unknown';
-        this.rGaugeCmdTable = gTx._calibrationTable
+        this.rGaugeCmdTable = this.gTx._calibrationTable
         this._okToSend = true;
         this.config = Config;
-        this.bPrl = new BLEperipheral(Config.dBusName, Config.uuid, this.bleMasterConfig, false);
-        self = this;
-        
+        this.gTx = new irTransmitter(this.gaugeIrAddress, this.calibrationTable);
+        this.bPrl = new BLEperipheral(this.dBusName, this.uuid, this._bleConfig, false);
+        self = this;  
     };
+
+    _bleConfig(DBus){
+        self.bleMasterConfig();
+        self.bleMyConfig();
+    }
 
     setGaugeValue(value){
         if(this._okToSend){
-            gTx.sendValue(value);
+            this.gTx.sendValue(value);
         } else {
             this.setGaugeStatus('Warining: Gauge value transmission not allowed during adminstration.')
             return false;
@@ -64,12 +71,12 @@ class gaugeConfig extends EventEmitter{
         console.log('No user specfic configuraton for appManger using default characteristics only.');
     }
 
-    bleMasterConfig(DBus){
+    _bleMasterConfig(){
         this.bPrl.logCharacteristicsIO = true;
         console.log('Initialize charcteristics...')
-        gaugeStatus =       self.bPrl.Characteristic('00000001-fe9e-4f7b-b56a-5f8294c6d817', 'gaugeStatus', ["encrypt-read","notify"]);
-        gaugeValue =        self.bPrl.Characteristic('00000002-fe9e-4f7b-b56a-5f8294c6d817', 'gaugeValue', ["encrypt-read","notify"]);
-        var gaugeCommand =  self.bPrl.Characteristic('00000003-fe9e-4f7b-b56a-5f8294c6d817', 'gaugeCommand', ["encrypt-write"]);
+        gaugeStatus =       this.bPrl.Characteristic('00000001-fe9e-4f7b-b56a-5f8294c6d817', 'gaugeStatus', ["encrypt-read","notify"]);
+        gaugeValue =        this.bPrl.Characteristic('00000002-fe9e-4f7b-b56a-5f8294c6d817', 'gaugeValue', ["encrypt-read","notify"]);
+        var gaugeCommand =  this.bPrl.Characteristic('00000003-fe9e-4f7b-b56a-5f8294c6d817', 'gaugeCommand', ["encrypt-write"]);
     
         console.log('Registering event handlers...');
         gaugeCommand.on('WriteValue', (device, arg1)=>{
@@ -80,33 +87,33 @@ class gaugeConfig extends EventEmitter{
             switch (cmdNum) {
                 case 0:
                     console.log('Sending test battery to gauge...');
-                    gTx.sendEncodedCmd(gTx.encodeCmd(gTx._cmdList.Check_Battery_Voltage));
+                    this.gTx.sendEncodedCmd(this.gTx.encodeCmd(this.gTx._cmdList.Check_Battery_Voltage));
                 break;
         
                 case 1:
                     console.log('Sending gauge reset request ');
-                    gTx.sendEncodedCmd(gTx.encodeCmd(gTx._cmdList.Reset));
+                    this.gTx.sendEncodedCmd(this.gTx.encodeCmd(this.gTx._cmdList.Reset));
                 break;
     
                 case 2:
                     console.log('Sending gauge Zero Needle request ');
-                    gTx.sendEncodedCmd(gTx.encodeCmd(gTx._cmdList.Zero_Needle));
+                    this.gTx.sendEncodedCmd(this.gTx.encodeCmd(this.gTx._cmdList.Zero_Needle));
                 break;          
         
                 case 15:
                     console.log('Sending Identifify gauge request')
-                    gTx.sendEncodedCmd(gTx.encodeCmd(gTx._cmdList.Identifify));
+                    this.gTx.sendEncodedCmd(this.gTx.encodeCmd(this.gTx._cmdList.Identifify));
                 break;
     
                 case 20:
                     console.log('Disable normal gauge value TX during adminstration.')
-                    self._okToSend = false;
-                    gTx.sendEncodedCmd(0);
+                    this._okToSend = false;
+                    this.gTx.sendEncodedCmd(0);
                 break;
         
                 case 21:
                     console.log('Enable normal gauge value TX.')
-                    self._okToSend = true;
+                    this._okToSend = true;
                 break;
             
                 default:
@@ -116,10 +123,8 @@ class gaugeConfig extends EventEmitter{
           });   
         
         console.log('setting default characteristic values...');
-        gaugeValue.setValue(self.value);
-        gaugeStatus.setValue(self.status)
-
-        self.bleMyConfig();
+        gaugeValue.setValue(this.value);
+        gaugeStatus.setValue(this.status)
     };
 
     saveItem(itemsToSaveAsObject){
@@ -147,28 +152,4 @@ class gaugeConfig extends EventEmitter{
 
 };
 
-/*
-function saveItem(itemsToSaveAsObject){
-    console.log('saveItem called with:');
-    console.log(itemsToSaveAsObject);
-
-    var itemList = Object.keys(itemsToSaveAsObject);
-    itemList.forEach((keyName)=>{
-        modifiedConfigMaster[keyName] = itemsToSaveAsObject[keyName];
-    })
-    console.log('Writting file to ' + modifiedConfigFilePath);
-    fs.writeFileSync(modifiedConfigFilePath, JSON.stringify(modifiedConfigMaster));
-    reloadConfig();
-};
-
-function reloadConfig(){
-    console.log('config reloading...');
-    if (fs.existsSync(modifiedConfigFilePath)){
-        modifiedConfigMaster = JSON.parse(fs.readFileSync(modifiedConfigFilePath))
-    };
-    Config = {...defaultGaugeConfig, ...modifiedConfigMaster}
-    console.log('firing "Update" event...');
-    self.emit('Update');
-};
-*/
 module.exports = gaugeConfig;
