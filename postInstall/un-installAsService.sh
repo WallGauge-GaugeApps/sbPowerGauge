42a63eb931532ffd8ee78a4fe94aebef0f03544f
John Rucker <John.Rucker@Solar-Current.com>
2019-01-30 10:45:09 -0600

    simulating error in un-install script

--- postInstall/un-installAsService.sh
+++ postInstall/un-installAsService.sh
@@ -2,7 +2,7 @@
 # To make this file executable follow these steps https://stackoverflow.com/questions/21691202/how-to-create-file-execute-mode-permissions-in-git-on-windows
 echo "un-install service will stop and remove this app as a service starts now..."
 echo "Stop service -> systemctl stop sbPowerGauge.service"
-sudo systemctl stop sbPowerGauge.service
+sudo systemctl stop ssbPowerGauge.service
 echo "Disable servers so it will not start on reboot ->systemctl disable sbPowerGauge.service"
 sudo systemctl disable sbPowerGauge.service
 echo "Remove the systemd service file -> rm /etc/systemd/system/sbPowerGauge.service"
