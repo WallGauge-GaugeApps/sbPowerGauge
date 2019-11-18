077977ccad14e79eb2351a0cd570f375c2c46041
John Rucker <JHRucker@outlook.com>
2019-11-18 14:27:49 -0600

    .

--- app.js
+++ app.js
@@ -70,9 +70,9 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-  };
+};
 
-  setTimeout(()=>{
-      console.log('sending error test');
-      myAppMan.sendAlert({['Sunnyboy WebBox']:"1"});
-  },30000);
+setTimeout(()=>{
+    console.log('sending error test');
+    myAppMan.sendAlert(JSON.stringify({['Sunnyboy WebBox']:"1"}));
+},30000);
